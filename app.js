723f62f52b5fe338d8f736391d41d7e2086f900c
Ankush-25 <repppol.me@gmail.com>
2025-04-12 17:17:57 +0530

    completed code

--- app.js
+++ app.js
@@ -4,35 +4,77 @@ let userSequence = [];
 let started = false;
 let level = 0;
 
-let btn = ['pink','orange','green','blue']
+let btn = ['pink', 'orange', 'green', 'blue']
 
 let Levelbar = document.querySelector('.currlevel');
 
-document.addEventListener("keypress",function(){
-    if(started === false){
-        console.log("The game has been Started");
+document.addEventListener("keypress", function () {
+    if (started === false) {
         started = true;
+
+        levelUp();
     }
 });
 
-function buttonFlash(btn){
+function gamebuttonFlash(btn) {
     btn.classList.add('flashbtn');
-    setTimeout(()=>{
+    setTimeout(() => {
         btn.classList.remove('flashbtn')
-    },1000);
+    }, 250);
 };
 
-function levelUp(){
-    if(started === true){
-        level++;    
-    }
-    Levelbar.innerText(`Level${level}`);
-    
-    let randomIndx = Math.floor(Math.random()*3);
-    let randomCol = btn[randomIndx];
-    let randombtn = document.querySelector(`".${randomCol}"`)
-    buttonFlash(randombtn);
+function userFlash(btn) {
+    btn.classList.add('userflashbtn');
+    setTimeout(() => {
+        btn.classList.remove('userflashbtn')
+    }, 444);
+};
 
+function levelUp() {
+    userSequence = [];
+    if (started === true) {
+        level++;
+    }
+    Levelbar.innerText = `Level ${level}`;
+
+    let randomIndx = Math.floor(Math.random() * btn.length);
+    let randomCol = btn[randomIndx];
+    let randombtn = document.querySelector(`.${randomCol}`)
+    gamebuttonFlash(randombtn);
+    gameSequence.push(randomCol);
 }
 
-levelUp();
+function userBoxPress() {
+    let box = this;
+    userFlash(box);
+    userSequence.push(this.classList[1]);
+    
+    console.log(userSequence);
+    userResponseCheck(userSequence.length-1);
+}
+
+let allboxes = document.querySelectorAll
+('.Box');
+
+for (allbox of allboxes) {
+    allbox.addEventListener('click', userBoxPress)
+}
+
+function userResponseCheck(idx) {
+    console.log("curr level :",level);
+    console.log(idx)
+    if(gameSequence[idx]== userSequence[idx]){
+     if (gameSequence.length === userSequence.length) {
+        setTimeout(()=>{levelUp()},1000)
+    }
+    }else{
+        Levelbar.innerText = "Game Over: Try Again by clicking any Key";
+        gameReset();
+    }
+}
+
+function gameReset(){
+    started = false;
+    level = 0;
+    gameSequence = []
+}
